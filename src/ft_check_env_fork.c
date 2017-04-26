95604a7b95f53e884a41f700c371de43f9c93c30
Nawel BELOUNIS <nbelouni@e1r11p5.42.fr>
2017-04-26 22:56:06 +0200

    ft_env truc TRUE->FALSE et vice et versa

--- src/ft_check_env_fork.c
+++ src/ft_check_env_fork.c
@@ -6,7 +6,7 @@
 /*   By: nbelouni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/26 18:07:40 by nbelouni          #+#    #+#             */
-/*   Updated: 2017/04/26 18:10:07 by nbelouni         ###   ########.fr       */
+/*   Updated: 2017/04/26 22:50:07 by nbelouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 int					ft_env_binary(char **args)
 {
 	int	*opt;
+	int	i;
 
 	opt = NULL;
 	if ((opt = ft_opt_parse(ENV_OPT, args + 1, 0, 1)) == NULL)
 		return (0);
-	if (opt[0] < 0 || !args[opt[0] + 1])
+	if (opt[0] < 0)
 	{
 		(opt) ? free(opt) : 0;
 		return (0);
 	}
+	i = opt[0];
+	while (args[++i])
+	{
+		if (args[i] && !ft_strchr(args[i], '='))
+		{
+			(opt) ? free(opt) : 0;
+			return (1);
+		}
+	}
 	(opt) ? free(opt) : 0;
-	return (1);
+	return (0);
 }
